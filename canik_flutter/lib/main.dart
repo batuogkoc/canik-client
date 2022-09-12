676fd57373b021223ebdcfd7b01e3fb4e2dc6ba5
batuogkoc <bgunduz21@ku.edu.tr>
2022-09-12 14:04:57 +0300

    Created scanning functionality

--- canik_flutter/lib/main.dart
+++ canik_flutter/lib/main.dart
@@ -1,115 +1,146 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import "package:flutter/foundation.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(const CanikApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+// Future<int> test() async {
+//   FlutterBluePlus flutterBlue = FlutterBluePlus.instance;
+//   // Start scanning
+//   flutterBlue.startScan(timeout: Duration(seconds: 4));
+//   // Listen to scan results
+//   var subscription = flutterBlue.scanResults.listen((results) async {
+//     // do something with scan results
+//     for (ScanResult r in results) {
+//       // if (r.device.name == "MyCanik ") {
+//       //   flutterBlue.stopScan();
+//       //   print('${r.device.name} found! rssi: ${r.rssi} id ${r.device.id}');
+//       //   await r.device.connect();
+//       // }
+//     }
+//   });
 
-  // This widget is the root of your application.
+//   // Stop scanning
+
+//   return 0;
+// }
+
+class CanikApp extends StatelessWidget {
+  const CanikApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Canik Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: StreamBuilder<BluetoothState>(
+        stream: FlutterBluePlus.instance.state,
+        initialData: BluetoothState.unknown,
+        builder: (context, snapshot) {
+          final state = snapshot.data;
+          if (state == BluetoothState.on) {
+            return const HomePage();
+          } else {
+            return const BluetoothOffScreen();
+          }
+        },
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class BluetoothOffScreen extends StatelessWidget {
+  const BluetoothOffScreen({Key? key, this.state}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final BluetoothState? state;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      backgroundColor: Colors.lightBlue,
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Icon(
+              Icons.bluetooth_disabled,
+              size: 200.0,
+              color: Colors.white54,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Bluetooth Adapter is ${state != null ? state.toString().substring(15) : 'not available'}.',
+              style: Theme.of(context)
+                  .primaryTextTheme
+                  .subtitle2
+                  ?.copyWith(color: Colors.white),
+            ),
+            ElevatedButton(
+              child: const Text('TURN ON'),
+              onPressed: defaultTargetPlatform == TargetPlatform.android
+                  ? () => FlutterBluePlus.instance.turnOn()
+                  : null,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Flutter Blue Plus Test")),
+        body: Column(
+          children: [
+            Center(
+              child: StreamBuilder<bool>(
+                stream: FlutterBluePlus.instance.isScanning,
+                initialData: false,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("Error with scanning");
+                  }
+                  final scanningState = snapshot.data ?? false;
+                  return ElevatedButton(
+                      onPressed: () {
+                        if (scanningState == true) {
+                          FlutterBluePlus.instance.stopScan();
+                        } else {
+                          FlutterBluePlus.instance
+                              .startScan(timeout: const Duration(seconds: 4));
+                        }
+                      },
+                      child: scanningState
+                          ? const Text("Stop scan")
+                          : const Text("Start scan"));
+                },
+              ),
+            ),
+            StreamBuilder<List<ScanResult>>(
+              stream: FlutterBluePlus.instance.scanResults,
+              initialData: const [],
+              builder: (context, snapshot) {
+                return Column(
+                    children: snapshot.data!
+                        .map((e) => TextButton(
+                              child: Text(
+                                  "${e.device.name == "" ? "<No device name>" : e.device.name} : ${e.device.id} , ${e.rssi}"),
+                              onPressed: () {
+                                e.device.connect();
+                              },
+                            ))
+                        .toList());
+              },
+            ),
+          ],
+        ));
   }
 }
