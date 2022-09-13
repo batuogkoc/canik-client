66214426d52d4c05e55fdb474b322ac464cfa335
batuogkoc <bgunduz21@ku.edu.tr>
2022-09-13 12:21:02 +0300

    finished frontend prototype, begun work on backend

---
+++ canik_flutter/lib/canik_backend.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import "dart:async";
+
+class CanikDevice {
+  final BluetoothDevice _device;
+
+  static const Map<String, List<String>> serviceCharacteristicUUIDMapping = {
+    "00001801-0000-1000-8000-00805f9b34fb": [
+      "00002a05-0000-1000-8000-00805f9b34fb"
+    ],
+    "00001800-0000-1000-8000-00805f9b34fb": [
+      "00002a00-0000-1000-8000-00805f9b34fb",
+      "00002a01-0000-1000-8000-00805f9b34fb",
+      "00002a04-0000-1000-8000-00805f9b34fb",
+    ],
+    "0000fe40-cc7a-482a-984a-7f2ed5b3e58f": [
+      "0000fe41-8e22-4541-9d4c-21edae82ed19",
+      "0000fe42-8e22-4541-9d4c-21edae82ed19",
+    ],
+    "00000001-0000-1000-8000-00805f9b34fb": [
+      "00000002-0000-1000-8000-00805f9b34fb",
+      "00000003-0000-1000-8000-00805f9b34fb",
+      "00000004-0000-1000-8000-00805f9b34fb",
+    ],
+    "00000005-0000-1000-8000-00805f9b34fb": [
+      "00000006-0000-1000-8000-00805f9b34fb",
+      "0000fe11-8e22-4541-9d4c-21edae82ed19",
+    ],
+    "0000fe20-cc7a-482a-984a-7f2ed5b3e58f": [
+      "0000fe22-8e22-4541-9d4c-21edae82ed19",
+      "0000fe23-8e22-4541-9d4c-21edae82ed19",
+      "0000fe24-8e22-4541-9d4c-21edae82ed19",
+    ],
+    "00000000-000e-11e1-9ab4-0002a5d5c51d": [
+      "00000001-000e-11e1-ac36-0002a5d5c51d",
+      "00000002-000e-11e1-ac36-0002a5d5c51d"
+    ]
+  };
+
+  late List<BluetoothService> services;
+
+  CanikDevice(this._device);
+  Future<void> connect() async {
+    await _device.connect();
+    services = await _device.discoverServices();
+
+    for (var service in services) {
+      if (serviceCharacteristicUUIDMapping
+              .containsKey(service.uuid.toString()) !=
+          true) {
+        throw Exception("The bluetooth device is not a Canik device");
+      }
+      for (var characteristic in service.characteristics) {
+        if (serviceCharacteristicUUIDMapping[service.uuid.toString()]
+                ?.contains(characteristic.uuid.toString()) !=
+            true) {
+          throw Exception("The bluetooth device is not a Canik device");
+        }
+      }
+    }
+  }
+}
--- canik_flutter/lib/main.dart
+++ canik_flutter/lib/main.dart
@@ -98,49 +98,128 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Flutter Blue Plus Test")),
-        body: Column(
-          children: [
-            Center(
-              child: StreamBuilder<bool>(
-                stream: FlutterBluePlus.instance.isScanning,
-                initialData: false,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: StreamBuilder<bool>(
+                  stream: FlutterBluePlus.instance.isScanning,
+                  initialData: false,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return const Text("Error with scanning");
+                    }
+                    final scanningState = snapshot.data ?? false;
+                    return ElevatedButton(
+                        onPressed: () {
+                          if (scanningState == true) {
+                            FlutterBluePlus.instance.stopScan();
+                          } else {
+                            FlutterBluePlus.instance.startScan(
+                                timeout: const Duration(seconds: 40));
+                          }
+                        },
+                        child: scanningState
+                            ? const Text("Stop scan")
+                            : const Text("Start scan"));
+                  },
+                ),
+              ),
+              StreamBuilder<List<ScanResult>>(
+                stream: FlutterBluePlus.instance.scanResults,
+                initialData: const [],
                 builder: (context, snapshot) {
-                  if (snapshot.hasError) {
-                    return const Text("Error with scanning");
-                  }
-                  final scanningState = snapshot.data ?? false;
-                  return ElevatedButton(
-                      onPressed: () {
-                        if (scanningState == true) {
-                          FlutterBluePlus.instance.stopScan();
-                        } else {
-                          FlutterBluePlus.instance
-                              .startScan(timeout: const Duration(seconds: 4));
-                        }
-                      },
-                      child: scanningState
-                          ? const Text("Stop scan")
-                          : const Text("Start scan"));
+                  return Column(
+                      children: snapshot.data!
+                          .map((e) => TextButton(
+                                child: Text(
+                                    "${e.device.name == "" ? "<No device name>" : e.device.name} : ${e.device.id} , ${e.rssi}"),
+                                onPressed: () {
+                                  Navigator.push(context, MaterialPageRoute(
+                                    builder: (context) {
+                                      FlutterBluePlus.instance.stopScan();
+                                      return DevicePage(device: e.device);
+                                    },
+                                  ));
+                                },
+                              ))
+                          .toList());
                 },
               ),
-            ),
-            StreamBuilder<List<ScanResult>>(
-              stream: FlutterBluePlus.instance.scanResults,
-              initialData: const [],
-              builder: (context, snapshot) {
-                return Column(
-                    children: snapshot.data!
-                        .map((e) => TextButton(
-                              child: Text(
-                                  "${e.device.name == "" ? "<No device name>" : e.device.name} : ${e.device.id} , ${e.rssi}"),
-                              onPressed: () {
-                                e.device.connect();
-                              },
-                            ))
-                        .toList());
-              },
-            ),
-          ],
+            ],
+          ),
         ));
+  }
+}
+
+class DevicePage extends StatelessWidget {
+  final BluetoothDevice device;
+  const DevicePage({Key? key, required this.device}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Canik: ${device.id}")),
+      body: FutureBuilder(
+        future: device.connect(timeout: const Duration(seconds: 4)),
+        // initialData: BluetoothDeviceState.connecting,
+        builder: (context, snapshot) {
+          print(snapshot);
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Column(
+                children: [
+                  const Text("Connection error"),
+                  const Icon(Icons.error_outline)
+                ],
+              );
+            }
+            return Center(
+              child: Column(
+                children: [
+                  ElevatedButton(
+                    child: const Text("Disconnect"),
+                    onPressed: () {
+                      device.disconnect();
+                      Navigator.pop(context);
+                    },
+                  ),
+                  FutureBuilder<List<BluetoothService>>(
+                    future: device.discoverServices(),
+                    // initialData: const [],
+                    builder: ((context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.hasError) {
+                          return const Text("Error while fetching services");
+                        }
+                        return Column(
+                            children: snapshot.data!.map((e) {
+                          print("S: ${e.uuid}");
+                          e.characteristics.forEach(
+                            (element) {
+                              print("C: ${element.uuid}");
+                            },
+                          );
+                          return Text("${e.uuid}");
+                        }).toList());
+                      } else {
+                        return const Text("Fetching services");
+                      }
+                    }),
+                  )
+                ],
+              ),
+            );
+          } else {
+            return Column(
+              children: [
+                const Text("Connecting..."),
+                const Icon(Icons.error_outline)
+              ],
+            );
+          }
+        },
+      ),
+    );
   }
 }
