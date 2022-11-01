c3ce53e590dd668bedf0f8fef901bfc47202750a
Can Karaçelebi <37347701+cankaracelebi@users.noreply.github.com>
2022-11-02 00:01:36 +0300

    mesaj

--- dart/shot_detection_test/test/shot_detection_test_test.dart
+++ dart/shot_detection_test/test/shot_detection_test_test.dart
@@ -1,6 +1,7 @@
 import 'package:shot_detection_test/shot_detection_test.dart';
 import 'package:test/test.dart';
 
+var deneme = 0;
 void main() {
   // test('calculate', () {
   //   expect(calculate(), 42);
