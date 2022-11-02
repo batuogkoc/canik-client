c1edfe79d44ee4e30561cd69b82ecb2cd4e7de7f
batuogkoc <bgunduz21@ku.edu.tr>
2022-11-02 11:55:55 +0300

    made a crude fusion algorithm tester

---
+++ dart/shot_detection_test/bin/fusion_test.dart
@@ -0,0 +1,5 @@
+import 'package:shot_detection_test/fusion_test.dart' as fusion_test;
+
+void main(List<String> arguments) {
+  fusion_test.main(arguments);
+}
---
+++ dart/shot_detection_test/lib/fusion_test.dart
@@ -0,0 +1,62 @@
+import 'package:csv/csv.dart';
+import 'package:vector_math/vector_math.dart';
+import "dart:io";
+import 'dart:convert';
+import 'package:canik_lib/canik_lib.dart';
+
+var rawDataToProcessedDataTransformer = RawDataToProcessedDataTransformer();
+
+void main(List<String> args) {
+  String path;
+  if (args.isEmpty) {
+    path = "../../test_data/DATA.csv";
+  } else {
+    path = args[0];
+  }
+  csvToProccessedData(path);
+}
+
+void csvToProccessedData(String path) async {
+  if (!FileSystemEntity.isFileSync(path)) {
+    stderr.write("No file at $path \n");
+    return;
+  }
+  double time = 0;
+  var stream = File(path)
+      .openRead()
+      .transform(utf8.decoder)
+      .transform(CsvToListConverter())
+      .where((event) {
+    return event.every((element) {
+      try {
+        element as double;
+      } catch (e) {
+        return false;
+      }
+      return true;
+    });
+  }).map((dynamicList) {
+    List<double> list = dynamicList.cast<double>();
+    var accel = Vector3(list[0], list[1], list[2]) / 1000;
+    var gyro = Vector3(list[3], list[4], list[5]);
+    double dt = list[6];
+    var euler = Vector3(list[7], list[8], list[9]);
+    time += dt;
+    return [RawData(accel, gyro, time), euler];
+  }).map((data) {
+    RawData rawData = data[0] as RawData;
+    Vector3 euler = data[1] as Vector3;
+    rawDataToProcessedDataTransformer.proccessRawData(rawData);
+    Quaternion validation = Quaternion.euler(euler[2], euler[1], euler[0]);
+    return (validation.inverted() *
+                    rawDataToProcessedDataTransformer.ahrs.quaternion)
+                .radians *
+            radians2Degrees -
+        (quaternionToEuler(rawDataToProcessedDataTransformer.ahrs.quaternion) *
+                    radians2Degrees -
+                euler)
+            .length;
+  });
+
+  stream.forEach(print);
+}
