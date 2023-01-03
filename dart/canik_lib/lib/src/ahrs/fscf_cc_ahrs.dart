b4ad3a2d3c570355ac529cdb8e321c9540470364
batuogkoc <bgunduz21@ku.edu.tr>
2023-01-03 15:20:31 +0300

    finished fscf_cc, fusion finished

--- dart/canik_lib/lib/src/ahrs/fscf_cc_ahrs.dart
+++ dart/canik_lib/lib/src/ahrs/fscf_cc_ahrs.dart
@@ -23,7 +23,46 @@ class FscfCcAhrs implements Ahrs {
 
   @override
   void updateIMU(Vector3 gyroRad, Vector3 accel, double dt) {
-    throw UnimplementedError("updateImu of fscf_cc not implemented");
+    accel = accel.clone();
+    gyroRad = gyroRad.clone();
+
+    accel.normalize();
+    _quaternion.normalize();
+    Quaternion qDot =
+        _quaternion * Quaternion(gyroRad.x, gyroRad.y, gyroRad.z, 0);
+    qDot = qDot.scaled(0.5);
+    // Quaternion qDot = Quaternion.dq(_quaternion, gyroRad);
+
+    Quaternion qp = _quaternion + qDot.scaled(dt);
+    // qp.normalize();
+
+    // Matrix3 R = Matrix3.zero();
+    // R.setRow(
+    //     0,
+    //     Vector3(2 * (0.5 - qp.y * qp.y - qp.z * qp.z), 0,
+    //         2 * (qp.x * qp.z - qp.w * qp.y)));
+    // R.setRow(
+    //     1,
+    //     Vector3(2 * (qp.x * qp.y - qp.w * qp.z), 0,
+    //         2 * (qp.w * qp.x + qp.y * qp.z)));
+    // R.setRow(
+    //     2,
+    //     Vector3(2 * (qp.w * qp.y + qp.x * qp.z), 0,
+    //         2 * (0.5 - qp.x * qp.x - qp.y * qp.y)));
+
+    Vector3 accMesPred = Vector3(2 * (qp.x * qp.z - qp.w * qp.y),
+        2 * (qp.w * qp.x + qp.y * qp.z), 2 * (0.5 - qp.x * qp.x - qp.y * qp.y));
+
+    final veca = accel.cross(accMesPred).normalized();
+    final aPlusm = veca * (aLambda1 / 2);
+    final qCor = Quaternion(aPlusm.x, aPlusm.y, aPlusm.z, 1);
+    Quaternion quat = qp * qCor;
+
+    if (quat.w < 0) {
+      quat = -quat;
+    }
+
+    _quaternion = quat.normalized();
   }
 
   @override
@@ -34,13 +73,13 @@ class FscfCcAhrs implements Ahrs {
 
     accel.normalize();
     mag.normalize();
-    _quaternion.normalize();
-    // Quaternion qDot =
-    //     _quaternion * Quaternion(gyroRad.x, gyroRad.y, gyroRad.z, 0);
-    Quaternion qDot = Quaternion.dq(_quaternion, gyroRad);
+    Quaternion qDot =
+        _quaternion * Quaternion(gyroRad.x, gyroRad.y, gyroRad.z, 0);
+    qDot = qDot.scaled(0.5);
+    // Quaternion qDot = Quaternion.dq(_quaternion, gyroRad);
 
     Quaternion qp = _quaternion + qDot.scaled(dt);
-    qp.normalize();
+    // qp.normalize();
 
     Matrix3 R = Matrix3.zero();
     R.setRow(
@@ -56,10 +95,10 @@ class FscfCcAhrs implements Ahrs {
         Vector3(2 * (qp.w * qp.y + qp.x * qp.z), 0,
             2 * (0.5 - qp.x * qp.x - qp.y * qp.y)));
 
-    Vector3 accMesPred = R.getColumn(2).normalized();
-    Vector3 h = _quaternion.rotate(mag);
+    Vector3 accMesPred = R.getColumn(2);
+    // Vector3 h = _quaternion.rotate(mag);
     double mr_z = accMesPred.dot(mag);
-    double mr_x = sqrt((1 - mr_z * mr_z).clamp(0.0, 1.0));
+    double mr_x = sqrt(1 - mr_z * mr_z);
     Vector3 mr = Vector3(mr_x, 0, mr_z);
     Vector3 magMesPred = R * mr;
 
@@ -71,9 +110,6 @@ class FscfCcAhrs implements Ahrs {
 
     if (quat.w < 0) {
       quat = -quat;
-    }
-    if (quat.storage.any((element) => element.isNaN)) {
-      print("nan");
     }
 
     _quaternion = quat.normalized();
