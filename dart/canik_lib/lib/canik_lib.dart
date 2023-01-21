743d21ce20a2506f06955d21d59b7b69ad6c03e5
batuogkoc <bgunduz21@ku.edu.tr>
2023-01-21 17:33:43 +0300

    finished savitzky-golay filter class added it to canik_lib

--- dart/canik_lib/lib/canik_lib.dart
+++ dart/canik_lib/lib/canik_lib.dart
@@ -1,4 +1,3 @@
-/// More dartdocs go here.
 library canik_lib;
 
 export 'src/canik_data.dart';
@@ -9,5 +8,4 @@ export 'src/ahrs/scf_ahrs.dart';
 export 'src/ahrs/fscf_cc_ahrs.dart';
 export 'src/ahrs/ahrs.dart';
 export 'src/canik_viz.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/savitzky_golay_filter.dart';
--- dart/canik_lib/lib/src/savitzky_golay_filter.dart
+++ dart/canik_lib/lib/src/savitzky_golay_filter.dart
@@ -1,15 +1,12 @@
 import 'package:scidart/numdart.dart';
 import 'dart:math' as Math;
 
-import 'package:scidart/scidart.dart';
-
 enum PadType { circular, replicate, symmetric }
 
 Array padArray(Array input, int padLength,
     {PadType padType = PadType.replicate}) {
   Array ret = Array.fromArray(input);
   int inputLen = input.length;
-  print(padLength);
   for (int i = 0; i < padLength; i++) {
     switch (padType) {
       case PadType.circular:
@@ -21,8 +18,8 @@ Array padArray(Array input, int padLength,
         ret.add(input.last);
         break;
       case PadType.symmetric:
-        ret.insert(0, input[i]);
-        ret.add(input[inputLen - 1 - i]);
+        ret.insert(0, input[i + 1]);
+        ret.add(input[inputLen - 2 - i]);
         break;
       default:
         throw Exception("Invalid padType");
@@ -39,44 +36,32 @@ int factorial(int n) {
   return r;
 }
 
-Array savitzkyGolay(Array data, int windowSize, int polyOrder,
-    {double spacing = 1.0, int derivative = 0}) {
-  String pad = 'none';
-  PadType padType = PadType.replicate;
-  if (windowSize % 2 == 0 || windowSize < 5) {
-    throw RangeError(
-      'Invalid window size (should be odd and at least 5 integer number)',
-    );
-  }
-  if (derivative < 0) {
-    throw RangeError('Derivative should be a positive integer');
-  }
-  if (polyOrder < 1) {
-    throw RangeError('Polynomial should be a positive integer');
-  }
+class SavitzkyGolayFilter {
+  final int windowSize;
+  final int polyOrder;
+  final double spacing;
+  final int derivative;
+  final Array coefficients;
+  SavitzkyGolayFilter(this.windowSize, this.polyOrder,
+      {this.spacing = 1.0, this.derivative = 0})
+      : coefficients = SavitzkyGolayFilter.generateCoefficients(
+            windowSize, polyOrder,
+            spacing: spacing, derivative: derivative);
+  static Array generateCoefficients(int windowSize, int polyOrder,
+      {double spacing = 1.0, int derivative = 0}) {
+    if (windowSize % 2 == 0 || windowSize < 5) {
+      throw RangeError(
+        'Invalid window size (should be odd and at least 5 integer number)',
+      );
+    }
+    if (derivative < 0) {
+      throw RangeError('Derivative should be a positive integer');
+    }
+    if (polyOrder < 1) {
+      throw RangeError('Polynomial should be a positive integer');
+    }
+    double norm;
 
-  // let C, norm;
-  Array C;
-  double norm;
-  int step = (windowSize.toDouble() / 2).floor();
-
-  // if (pad == 'pre') {
-  //   print("pad pre");
-  //   data = padArray(data, step, padType: padType);
-  // }
-  // if (windowSize == 5 &&
-  //     polynomial == 2 &&
-  //     (derivative == 1 || derivative == 2)) {
-  //   if (derivative == 1) {
-  //     C = Array([-2, -1, 0, 1, 2]);
-  //     norm = 10;
-  //   } else {
-  //     C = Array([2, -1, -2, -1, 2]);
-  //     norm = 7;
-  //   }
-  // } else
-  {
-    // var J = Matrix.generate(windowSize, polynomial + 1);
     Array2d J = Array2d.fixed(windowSize, polyOrder + 1);
     double inic = -(windowSize - 1) / 2;
 
@@ -90,31 +75,34 @@ Array savitzkyGolay(Array data, int windowSize, int polyOrder,
       }
     }
 
-    var C_temp = matrixInverse(J);
-    C = C_temp[derivative];
+    var coefficientsTemp = matrixInverse(J);
+    Array coefficients = coefficientsTemp[derivative];
+
     norm = 1 / factorial(derivative);
-  }
-  double det = norm * Math.pow(spacing, derivative);
 
-  Array ans = Array.fixed(data.length);
-  // print(C);
-  data = padArray(data, step);
-  // print(data);
-  for (int k = 0; k < data.length - 2 * step; k++) {
-    double d = 0;
-    for (int l = 0; l < C.length; l++) {
-      d += (C[l] * data[l + k]) / det;
+    double det = norm * Math.pow(spacing, derivative);
+    for (int i = 0; i < coefficients.length; i++) {
+      coefficients[i] /= det;
     }
-    ans[k] = d;
+
+    return coefficients;
   }
 
-  // if (pad == 'post') {
-  //   print("pad post");
+  Array filter(Array signal, {PadType padType = PadType.replicate}) {
+    int step = (windowSize.toDouble() / 2).floor();
 
-  //   ans = padArray(ans, step, padType: padType);
-  // }
+    Array ans = Array.fixed(signal.length, initialValue: 0);
 
-  return ans;
+    signal = padArray(signal, step, padType: padType);
+    for (int k = 0; k < signal.length - 2 * step; k++) {
+      double d = 0;
+      for (int l = 0; l < coefficients.length; l++) {
+        d += (coefficients[l] * signal[l + k]);
+      }
+      ans[k] = d;
+    }
+    return ans;
+  }
 }
 
 void main(List<String> args) {
@@ -145,8 +133,8 @@ void main(List<String> args) {
     0.06683186311488976,
     0.7228077535608323
   ]);
-
-  var filtered = savitzkyGolay(data, 5, 2);
+  var filter = SavitzkyGolayFilter(5, 2);
+  var filtered = filter.filter(data, padType: PadType.symmetric);
 
   // print(data.length);
   // print(filtered.length);
